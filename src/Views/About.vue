cc9dcc9c0289c26f07ddbd4b40452fe4dfadc794
Xu Zirui <xuzirui@Xus-MacBook-Air-3.local>
2024-03-13 19:06:53 +0800

    initial commit

--- src/Views/About.vue
+++ src/Views/About.vue
@@ -1,19 +1,16 @@
 <template>
 
-<div class="header">
-    <h1> Welcome to CryptoTrack </h1>
+<div class="aboutus">
+    <h1> About us </h1>
 </div>
-
-<div class="about us">
-    <h2> About us </h2>
 
     <h3> CryptoTrack is a real-time cryptocurrency tracking website designed to empower investors, traders, and enthusiasts with up-to-the-minute information on the dynamic world of cryptocurrencies. </h3>
-</div>
 
-<div class="our mission"> 
-    <h2> Our Mission </h2>
-    <h3> Our mission is to provide you with accurate, comprehensive, and current data across all major cryptocurrencies, enabling you to make informed decisions with confidence. At CryptoTrack, we understand the importance of staying ahead in the fast-paced crypto market, which is why our platform combines cutting-edge technology with user-friendly design to bring you live updates, trend analyses, and detailed insights into market movements. Whether you're monitoring Bitcoin, Ethereum, or exploring emerging altcoins, CryptoTrack is your reliable partner in navigating the complexities of cryptocurrency investments. Join our community today and take the first step towards mastering the art of cryptocurrency trading. </h3>
+<div class="ourmission"> 
+    <h1> Our Mission </h1>
 </div>
+    <h3> Our mission is to provide you with accurate, comprehensive, and current data across all major cryptocurrencies, enabling you to make informed decisions with confidence. At CryptoTrack, we understand the importance of staying ahead in the fast-paced crypto market, which is why our platform combines cutting-edge technology with user-friendly design to bring you live updates, trend analyses, and detailed insights into market movements. Whether you're monitoring Bitcoin, Ethereum, or exploring emerging altcoins, CryptoTrack is your reliable partner in navigating the complexities of cryptocurrency investments. Join our community today and take the first step towards mastering the art of cryptocurrency trading. </h3>
+
 </template>
 
 <script>
@@ -23,7 +20,10 @@
 </script>
 
 <style>
-.header {
+.aboutus {
+    background-color: cornflowerblue;
+}
+.ourmission {
     background-color: cornflowerblue;
 }
 
